3598f6912844111fc0ea117e21380f13b54f8c26
Philipmaina <zakmaina@ymail.com>
2015-07-29 00:50:42 +0300

    I have changed the program to a module which takes any binary, octal and hexadecimal value and convert it to a decimal value

--- bin_to_dec_shortway.rb
+++ bin_to_dec_shortway.rb
@@ -1,35 +1,59 @@
 =begin
- 	THIS PROGRAM CONVERTS BINARY NUMBERS TO DECIMAL VALUES
+ 	THIS METHOD CONVERTS BINARY,OCTAL AND HEXADECIMAL NUMBERS TO DECIMAL VALUES
  	IN RUBY
 
  	We use a function that is almost equivalent to LEFT or RIGHT 
  	FUNCTION in vb where we can get substring from a string, in this 
  	case the substring is a character that we have to convert to a 
  	digit. We do this because we have to multiply each individual digit
- 	by the Base(2)^exponent.
+ 	by the Base(2,8,16)^exponent.
 
  	We also use the length function to limit our iteration 
 
  	The power(exponent operator) in ruby is **
 
+ 	entryvaluestring which is what the user inputs is a global variable
+
 =end 
 
-#PROGRAM STARTS HERE
+#METHOD(FUNCTION) STARTS HERE
 
-puts("Key in a binary number that you wish to be converted :")
-binarystring = gets.chomp #chomp get rid off the "\n" that would otherwise appear
+def Othersystem_to_decimal(base)
 
-i = 0
-finalans = 0
+	i = 0
+    finalans = 0
 
-while (i < binarystring.length)
+	while (i < $entryvaluestring.length)
 
-	finalans = (binarystring[(-1-i),1].to_i * (2**i)) + finalans
-	i = i + 1
-	
+		substrentryvalue = $entryvaluestring[(-1-i),1]
+
+		if substrentryvalue == "A"
+			substrentryvalue = "10"
+		elsif substrentryvalue == "B"
+			substrentryvalue = "11"
+		elsif substrentryvalue == "C"
+			substrentryvalue = "12"
+		elsif substrentryvalue == "D"
+			substrentryvalue = "13"
+		elsif substrentryvalue == "E"
+			substrentryvalue = "14"
+		elsif substrentryvalue == "F"
+			substrentryvalue = "15"
+		end
+
+		finalans = (substrentryvalue.to_i * (base**i)) + finalans
+
+		i = i + 1
+			
+			
+	end
+
+	return finalans
+
 end
 
-puts("The decimal value of the given binary number is #{ finalans }")
+
+
 
 
 
